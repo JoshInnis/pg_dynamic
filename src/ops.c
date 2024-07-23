302c8dd1c59e218ec8897df491a079dfb8f21f1a
josh <josh@ubunutupgdynamic.myguest.virtualbox.org>
2024-07-22 19:34:11 -0500

    Support Bitint Addition

--- src/ops.c
+++ src/ops.c
@@ -16,3 +16,36 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+#include "postgres.h"
+
+#include <math.h>
+
+#include "catalog/pg_type_d.h"
+#include "fmgr.h"
+#include "utils/builtins.h"
+#include "utils/numeric.h"
+#include "utils/timestamp.h"
+#include "varatt.h"
+
+#include "utils/dynamic.h"
+#include "dynamic_typecasting.h"
+
+PG_FUNCTION_INFO_V1(dynamic_add);
+Datum
+dynamic_add(PG_FUNCTION_ARGS) {
+    dynamic *lhs = AG_GET_ARG_DYNAMIC_P(0);
+    dynamic *rhs = AG_GET_ARG_DYNAMIC_P(1);
+
+    dynamic_value dyna_val = {
+        .type = DYNAMIC_INTEGER,
+        .val.int_value = DatumGetUInt64(DirectFunctionCall2(int8pl,
+           convert_to_scalar(dynamic_to_int8_internal, lhs, "dynamic integer"),
+           convert_to_scalar(dynamic_to_int8_internal, rhs, "dynamic integer")))
+    };
+
+    PG_FREE_IF_COPY(lhs, 0);
+    PG_FREE_IF_COPY(rhs, 0);
+    
+    AG_RETURN_DYNAMIC_P(dynamic_value_to_dynamic(&dyna_val));    
+}
